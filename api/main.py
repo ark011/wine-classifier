040c95ec7e9818715fa54fc76934b1ff3c71dae6
ark011 <artur.kolakowski@uconn.edu>
2020-07-28 14:42:26 -0400

    first commit

--- api/main.py
+++ api/main.py
@@ -2,33 +2,39 @@ import numpy as np
 import pandas as pd
 
 from typing import List
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
-from pydantic import BaseModel, ValidationError, validator
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel, validator
 
 from .ml.model import Model, get_model, num_attributes
 
+""" Base model class that is used to for prediction. validator included to make sure 
+	that the data point contains the right amount of attributes/features in order to predict
+"""
 class PredictRequest(BaseModel):
 	data: List[List[float]]
 
 	@validator("data")
-	def check_dimensionality(cls, v):
-		for data_point in v:
+	def check_dimensions(cls, incoming_data):
+		for data_point in incoming_data:
 			if len(data_point) != num_attributes:
 				raise ValueError(f"Each data point must contain {num_attributes} attributes/features")
-		return v
+		return incoming_data
 
+
+""" Base model class that is used to return the data """
 class PredictResponse(BaseModel):
 	data: List[str]
 
 
 app = FastAPI()
 
+""" Display Welcome message on home endpoint"""
 @app.get('/')
 def read_root():
 	return "Welcome!"
 
 
+""" POST METHOD to pass in the data and predict whether its red or white wine"""
 @app.post("/predict", response_model=PredictResponse)
 def predict(input: PredictRequest, model: Model = Depends(get_model)):
 	X = np.array(input.data)
--- api/ml/model.py
+++ api/ml/model.py
@@ -1,33 +1,41 @@
 import pickle
 import numpy as np
 import pandas as pd
-from pathlib import Path
 
-from sklearn.model_selection import train_test_split
+from pathlib import Path
 from sklearn.linear_model import LogisticRegression
 
 class Model:
 
+	""" initialize model and its fields """
 	def __init__(self, model_path: str = None):
 		self._model = None
 		self._model_path = model_path
 		self.load()
 
 
+	""" train function, takes in two numpy arrays
+		X - attributes
+		y - label
+		Logistic Regression model is loaded and model is fitted with X and y
+	"""
 	def train(self, X: np.ndarray, y : np.ndarray):
 		self._model = LogisticRegression()
 		self._model.fit(X,y)
 		return self
 
+	""" predict function, takes in X numpy array, returns prediction """
 	def predict(self, X: np.ndarray) -> np.ndarray:
 		return self._model.predict(X)
 
+	""" save function, to save the model if model does not exist, raises an error """
 	def save(self):
 		if self._model is not None:
 			pickle.dump(self._model, open(self._model_path, 'wb'))
 		else:
 			raise TypeError("The model has not been trained, please include .train() in order to save the model")
 
+	""" load function, loads the model that has been trained into _model field """
 	def load(self):
 		try:
 			self._model = pickle.load(open(self._model_path, 'rb'))
@@ -38,19 +46,22 @@ class Model:
 		print(self._model)
 		return self
 
+# get models' path
 model_path = Path(__file__).parent / "model.pkl"
+
+# number of attributes, used in main.py to validate the data that is passed in 
+# has the correct number of attributes/features
 num_attributes = pd.read_csv('/api/ml/wine_combined.csv').shape[1]-1
+
+# call to instantiate Model class and creates instance
 model = Model(model_path)
 
 def get_model():
 	return model
 
-
 if __name__ == "__main__":
-	df = pd.read_csv('/api/ml/wine_combined.csv')
-	X = df.iloc[:,:-1]
-	y = np.array(df['wine_type'])
-	print(X.head())
-	print(y)
-	model.train(X,y)
-	model.save()
+	df = pd.read_csv('/api/ml/wine_combined.csv') 	# read csv file
+	X = df.iloc[:,:-1] 								# take X attributes/features
+	y = np.array(df['wine_type'])					# take y label
+	model.train(X,y)								# train model
+	model.save()									# save model
